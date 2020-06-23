3687d00365b3c6fcf8a12d646be9fe3ac1bc178b
Ruijie Fang <ruijief@princeton.edu>
2020-06-23 01:32:21 -0400

    add bash scripts

---
+++ gsoc-ruijief/run-prof.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+debootstrap_dir=debootstrap
+root_filesystem=debootstrap.ext2.qcow2
+linux_image="$(printf "${debootstrap_dir}/boot/vmlinuz-"*)"
+./qemu/x86_64-softmmu/qemu-system-x86_64 \
+  -append 'console=ttyS0 root=/dev/sda' \
+  -drive "file=${root_filesystem},format=qcow2" \
+  -enable-kvm \
+  -serial mon:stdio \
+  -m 2G \
+  -kernel "${linux_image}" \
+  -device rtl8139,netdev=net0 \
+  -netdev user,id=net0
---
+++ gsoc-ruijief/run.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+# supply valid qemu userspace via $1
+debootstrap_dir=debootstrap
+root_filesystem=debootstrap.ext2.qcow2
+linux_image="$(printf "${debootstrap_dir}/boot/vmlinuz-"*)"
+$1 \
+  -append 'console=ttyS0 root=/dev/sda' \
+  -drive "file=${root_filesystem},format=qcow2" \
+  -enable-kvm \
+  -serial mon:stdio \
+  -m 2G \
+  -kernel "${linux_image}" \
+  -device rtl8139,netdev=net0 \
+  -netdev user,id=net0
